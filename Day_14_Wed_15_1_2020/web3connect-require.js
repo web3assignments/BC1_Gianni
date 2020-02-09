89b58f1f5800a91a4be289da3c1f3ad34fe849e9
AkainuGianni <57665235+AkainuGianni@users.noreply.github.com>
2020-02-09 19:40:46 +0100

    update

--- Day_14_Wed_15_1_2020/web3connect-require.js
+++ Day_14_Wed_15_1_2020/web3connect-require.js
@@ -1,6 +1,3 @@
 Web3Connect = require('web3connect').default;
 WalletConnectProvider = require('@walletconnect/web3-provider').default;
 Web3 = require('web3');
-ENS         = require('ethereum-ens');
-namehash    = require('eth-ens-namehash');
-contentHash = require('content-hash');
